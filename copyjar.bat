c95dfed64e44f74f9b4a9aab42c5b4f29bc1ac6b
lzybluee <lzybluee@gmail.com>
2019-07-19 22:44:34 +0800

    Add copyjar.bat

---
+++ copyjar.bat
@@ -0,0 +1,7 @@
+mkdir forge
+xcopy /E /Y forge-ai\target\classes\forge forge
+xcopy /E /Y forge-core\target\classes\forge forge
+xcopy /E /Y forge-game\target\classes\forge forge
+xcopy /E /Y forge-gui\target\classes\forge forge
+xcopy /E /Y forge-gui-desktop\target\classes\forge forge
+pause
