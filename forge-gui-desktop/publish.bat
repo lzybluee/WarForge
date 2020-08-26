937d07849ee4d28e6dcca88cbb6cfd2617028a50
lzybluee <lzybluee@gmail.com>
2020-08-26 14:07:14 +0800

    Update maven project file

--- forge-gui-desktop/publish.bat
+++ forge-gui-desktop/publish.bat
@@ -1,2 +1,11 @@
+cd ../forge-core
+call mvn clean install
+cd ../forge-ai
+call mvn clean install
+cd ../forge-game
+call mvn clean install
+cd ../forge-gui
+call mvn clean install
+cd ../forge-gui-desktop
 call mvn -U -B clean -P windows-linux-release install
 pause
